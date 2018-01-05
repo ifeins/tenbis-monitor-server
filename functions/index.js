434deee6e7cf3f814ac608f2e494440040e89031
Ido Feins <ifeins@gmail.com>
2018-01-05 18:40:29 +0200

    Created first cloud function.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.updateUser = functions.https.onRequest((req, res) => {
+  const userId = req.body.userId;  
+  const phone = req.body.phone;
+
+  const docRef = db.collection('users').document(userId);
+  docRef.set({phone: phone});
+
+  res.sendStatus(200);
+});
